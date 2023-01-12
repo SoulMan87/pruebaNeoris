23c98a18c6f7e5b755728c4fcbe14cfb1d001ea4
jonathan <jonathanhinestroza87@gmail.com>
2023-01-12 12:45:14 -0500

    Cliente entity

--- src/main/java/com/soulrebel/neoris/entity/Cliente.java
+++ src/main/java/com/soulrebel/neoris/entity/Cliente.java
@@ -4,12 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.Table;
 
-@Table(name = "CLIENTE")
 @Entity
-@PrimaryKeyJoinColumn(name = "clienteId")
 @Setter
 @Getter
 public class Cliente extends Persona {
